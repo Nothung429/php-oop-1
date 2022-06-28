97306e80cc585814a7e8807625dfc3e14b4b1ee2
Nothung429 <frank.gatto96.fg@gmail.com>
2022-06-28 10:49:00 +0200

    fix php classes

---
+++ classes/MovieConstructor.php
@@ -0,0 +1,14 @@
+<?php
+    require_once __DIR__ . "/Movie.php";
+
+    $movies = [
+        new movie("Harry Potter and the Philosopher's Stone", "2001", "en", "7.6", "Chris Columbus", "Daniel Radcliffe, Emma Watson, Rupert Grint", "WB", "https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/0004/9055/brand.gif?itok=vGXqOgqt", 22),
+        new movie("Harry Potter and the Chamber of Secrets", "2002", "en", "7.4", "Chris Columbus", "Daniel Radcliffe, Emma Watson, Rupert Grint", "WB", "https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/0004/9055/brand.gif?itok=vGXqOgqt", 56),
+        new movie("Harry Potter and the Prisoner of Azkaban", "2004", "en", "7.2", "Alfonso Cuarón", "Daniel Radcliffe, Emma Watson, Rupert Grint", "WB", "https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/0004/9055/brand.gif?itok=vGXqOgqt", 65),
+        new movie("Harry Potter and the Goblet of Fire", "2005", "en", "7.0", "Mike Newell", "Daniel Radcliffe, Emma Watson, Rupert Grint", "WB", "https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/0004/9055/brand.gif?itok=vGXqOgqt", 70),
+        new movie("Harry Potter and the Order of the Phoenix", "2007", "en", "6.8", "David Yates", "Daniel Radcliffe, Emma Watson, Rupert Grint", "WB", "https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/0004/9055/brand.gif?itok=vGXqOgqt", 43),
+        new movie("Harry Potter and the Half-Blood Prince", "2019", "en", "6.6", "David Yates", "Daniel Radcliffe, Emma Watson, Rupert Grint", "WB", "https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/0004/9055/brand.gif?itok=vGXqOgqt", 89),
+        new movie("Harry Potter and the Deathly Hallows | Part 1", "2010", "en", "6.4", "David Yates", "Daniel Radcliffe, Emma Watson, Rupert Grint", "WB", "https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/0004/9055/brand.gif?itok=vGXqOgqt", 53),
+        new movie("Harry Potter and the Deathly Hallows | Part 2", "2011", "en", "6.2", "David Yates", "Daniel Radcliffe, Emma Watson, Rupert Grint", "WB", "https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/0004/9055/brand.gif?itok=vGXqOgqt", 96),
+    ]
+?>
--- index.php
+++ index.php
@@ -1,16 +1,5 @@
 <?php
-    require_once __DIR__ . "/classes/Movie.php";
-
-    $movies = [
-        new movie("Harry Potter and the Philosopher's Stone", "2001", "en", "7.6", "Chris Columbus", "Daniel Radcliffe, Emma Watson, Rupert Grint", "WB", "https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/0004/9055/brand.gif?itok=vGXqOgqt", 15),
-        new movie("Harry Potter and the Chamber of Secrets", "2002", "en", "7.4", "Chris Columbus", "Daniel Radcliffe, Emma Watson, Rupert Grint", "WB", "https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/0004/9055/brand.gif?itok=vGXqOgqt", 15),
-        new movie("Harry Potter and the Prisoner of Azkaban", "2004", "en", "7.2", "Alfonso Cuarón", "Daniel Radcliffe, Emma Watson, Rupert Grint", "WB", "https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/0004/9055/brand.gif?itok=vGXqOgqt", 15),
-        new movie("Harry Potter and the Goblet of Fire", "2005", "en", "7.0", "Mike Newell", "Daniel Radcliffe, Emma Watson, Rupert Grint", "WB", "https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/0004/9055/brand.gif?itok=vGXqOgqt", 15),
-        new movie("Harry Potter and the Order of the Phoenix", "2007", "en", "6.8", "David Yates", "Daniel Radcliffe, Emma Watson, Rupert Grint", "WB", "https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/0004/9055/brand.gif?itok=vGXqOgqt", 15),
-        new movie("Harry Potter and the Half-Blood Prince", "2019", "en", "6.6", "David Yates", "Daniel Radcliffe, Emma Watson, Rupert Grint", "WB", "https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/0004/9055/brand.gif?itok=vGXqOgqt", 15),
-        new movie("Harry Potter and the Deathly Hallows | Part 1", "2010", "en", "6.4", "David Yates", "Daniel Radcliffe, Emma Watson, Rupert Grint", "WB", "https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/0004/9055/brand.gif?itok=vGXqOgqt", 15),
-        new movie("Harry Potter and the Deathly Hallows | Part 2", "2011", "en", "6.2", "David Yates", "Daniel Radcliffe, Emma Watson, Rupert Grint", "WB", "https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/0004/9055/brand.gif?itok=vGXqOgqt", 15),
-    ]
+    require_once __DIR__ . "/classes/MovieConstructor.php";
 ?>
 
 <!DOCTYPE html>
